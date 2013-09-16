19250420981dc7e97273695a5ff8081e8a99a0af
Jake Marsh <jakemmarsh@gmail.com>
2013-09-15 21:25:53 -0400

    began laying out "post event" page

--- app/js/controllers/post-ctrl.js
+++ app/js/controllers/post-ctrl.js
@@ -1,6 +1,15 @@
 define(['./index'], function (controllers) {
     'use strict';
-    controllers.controller('postCtrl', function () {
-    	
+    controllers.controller('postCtrl', function ($scope) {
+    	$scope.mapOptions = {
+			center: new google.maps.LatLng(44.883125, -68.671977),
+			zoom: 15,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			disableDefaultUI: true,
+			disableDoubleClickZoom: true,
+			draggable: false,
+			scrollwheel: false,
+			panControl: false
+	    };
     });
 });
