26556935f0dd7d3c3d6699ca15ab70eeca29e974
Jake Marsh <jakemmarsh@gmail.com>
2013-09-17 15:01:06 -0400

    began laying out "settings" page

--- app/js/controllers/settings-ctrl.js
+++ app/js/controllers/settings-ctrl.js
@@ -1,6 +1,19 @@
 define(['./index'], function (controllers) {
     'use strict';
-    controllers.controller('settingsCtrl', function () {
-    	
+    controllers.controller('settingsCtrl', function ($scope, $modal) {
+    	$scope.open = function (modal) {
+		  	if (modal.toLowerCase() == 'twitter') {
+			    var modalInstance = $modal.open({
+			      templateUrl: 'twitterModal.html',
+			      controller: 'modalInstanceCtrl'
+			    });
+			}
+			else if (modal.toLowerCase() == 'facebook') {
+				var modalInstance = $modal.open({
+			      templateUrl: 'facebookModal.html',
+			      controller: 'modalInstanceCtrl'
+			    });
+			}
+		};
     });
 });
