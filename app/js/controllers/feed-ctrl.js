d4a734d8f91ff0466b88964af524611c033eacf0
Jake Marsh <jakemmarsh@gmail.com>
2013-09-25 15:03:34 -0400

    added modals to feed

--- app/js/controllers/feed-ctrl.js
+++ app/js/controllers/feed-ctrl.js
@@ -1,6 +1,18 @@
 define(['./index'], function (controllers) {
     'use strict';
-    controllers.controller('feedCtrl', function () {
-    	
+    controllers.controller('feedCtrl', function ($scope, $modal) {
+    	$scope.openRSVP = function (eventId) {
+		    var modalInstance = $modal.open({
+		      templateUrl: 'rsvpModal.html',
+		      controller: 'modalInstanceCtrl'
+		    });
+		};
+
+		$scope.openAttending = function (eventId) {
+		    var modalInstance = $modal.open({
+		      templateUrl: 'attendingModal.html',
+		      controller: 'modalInstanceCtrl'
+		    });
+		};
     });
 });
