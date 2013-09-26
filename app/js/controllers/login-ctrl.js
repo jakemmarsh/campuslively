f72fe264f499bfa4a0d2b6cc0c0422a3f5fa5f26
Jake Marsh <jakemmarsh@gmail.com>
2013-09-26 10:05:02 -0400

    styled login page, added dummy 'login' function

--- app/js/controllers/login-ctrl.js
+++ app/js/controllers/login-ctrl.js
@@ -1,6 +1,8 @@
 define(['./index'], function (controllers) {
     'use strict';
-    controllers.controller('loginCtrl', function () {
-    	
+    controllers.controller('loginCtrl', function ($scope) {
+    	$scope.login = function() {
+    		console.log('login');
+    	}
     });
 });
