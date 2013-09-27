9364f022862c164d4dba0180fe477f11ca8b139b
Jake Marsh <jakemmarsh@gmail.com>
2013-09-27 18:52:23 -0400

    made login function redirect to feed

--- app/js/controllers/login-ctrl.js
+++ app/js/controllers/login-ctrl.js
@@ -2,7 +2,7 @@ define(['./index'], function (controllers) {
     'use strict';
     controllers.controller('loginCtrl', function ($scope) {
     	$scope.login = function() {
-    		console.log('login');
+    		$location.path('/feed');
     	}
     });
 });
