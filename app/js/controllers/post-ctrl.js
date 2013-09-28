f154f0529cd8768d30bf531e6640357e9888794a
Jake Marsh <jakemmarsh@gmail.com>
2013-09-28 16:52:44 -0400

    added user's gps location to post page, added test function for foursquare api

--- app/js/controllers/post-ctrl.js
+++ app/js/controllers/post-ctrl.js
@@ -1,10 +1,23 @@
 define(['./index'], function (controllers) {
     'use strict';
-    controllers.controller('postCtrl', function ($scope) {
+    controllers.controller('postCtrl', function ($scope, locationService, resolvedLocation) {
+    	$scope.userPosition = resolvedLocation;
+
+    	var testFour = function(position) {
+    		locationService.foursquare(position).then(function (data) {
+				console.log(data);
+		    },
+		    function (errorMessage) {
+		        console.log(errorMessage);
+		    });
+    	}
+
+    	testFour($scope.userPosition);
+
     	$scope.eventPosted = false;
 
     	$scope.mapOptions = {
-			center: new google.maps.LatLng(44.883125, -68.671977),
+			center: new google.maps.LatLng($scope.userPosition.latitude, $scope.userPosition.longitude),
 			zoom: 15,
 			mapTypeId: google.maps.MapTypeId.ROADMAP,
 			disableDefaultUI: true,
