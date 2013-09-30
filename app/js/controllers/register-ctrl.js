1737449d39616b390d3081c14a3da10053dad16c
Jake Marsh <jakemmarsh@gmail.com>
2013-09-30 14:53:59 -0400

    added student or business option to register page

--- app/js/controllers/register-ctrl.js
+++ app/js/controllers/register-ctrl.js
@@ -2,6 +2,7 @@ define(['./index'], function (controllers) {
     'use strict';
     controllers.controller('registerCtrl', function ($scope) {
     	$scope.emailSent = false;
+    	$scope.userType = null;
 
     	$scope.schools = ['Boston University', 'University of Maine'];
 
