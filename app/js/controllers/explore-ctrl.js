f214a9f8721251293f58adda11616a40cdae603c
Jake Marsh <jakemmarsh@gmail.com>
2013-10-01 14:27:08 -0400

    added functionality to search bar

--- app/js/controllers/explore-ctrl.js
+++ app/js/controllers/explore-ctrl.js
@@ -62,7 +62,7 @@ define(['./index'], function (controllers) {
     		formattedDay: formatDate(new Date("June 10, 2013")),
     		time: '11:30pm',
     		description: 'Famous Miami-based DJ Hardo LIVE at Bijou tonight! Tickets are $20 at the door. Order now and get them for $15! www.bijounightclub.com',
-    		tags: ['club', 'live music'],
+    		tags: ['club'],
     		posted: new Date("June 8, 2013")
     	}];
 
