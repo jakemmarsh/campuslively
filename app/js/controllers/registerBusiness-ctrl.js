1faf43ef5b714430b892bcc0e1adb85dee4793d4
Jake Marsh <jakemmarsh@gmail.com>
2013-10-17 18:20:07 -0400

    added school selection to business registration

--- app/js/controllers/registerBusiness-ctrl.js
+++ app/js/controllers/registerBusiness-ctrl.js
@@ -1,6 +1,24 @@
 define(['./index'], function (controllers) {
     'use strict';
-    controllers.controller('registerBusinessCtrl', function ($scope, authService) {
+    controllers.controller('registerBusinessCtrl', function ($scope, authService, schoolService) {
+    	schoolService.getAllSchools().then(function (data, status) {
+    		$scope.schools = data;
+    	}, function(errorMessage, status) {
+    		console.log(errorMessage);
+    	});
+
+    	$scope.checkUsername = function() {
+    		authService.checkUsername($scope.username).then(function (isTaken, status) {
+    			if(isTaken == 'true') {
+    				console.log('username taken');
+    				$scope.usernameTaken = true;
+    			}
+    			else {
+    				$scope.usernameTaken = false;
+    			}
+    		});
+    	};
+
     	$scope.register = function() {
     		var newUser = {
     			type: 'business',
@@ -8,6 +26,7 @@ define(['./index'], function (controllers) {
     			password: $scope.password,
     			businessName: $scope.businessName,
     			businessDescription: $scope.businessDescription,
+    			school: $scope.school,
     			email: $scope.email
     		};
 
