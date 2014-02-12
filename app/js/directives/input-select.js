2b0f600f2a540170eab6d234b8403fa5cd987ff0
Jake Marsh <jakemmarsh@gmail.com>
2014-02-12 17:30:00 -0500

    changed event URL inputs to be readonly

--- app/js/directives/input-select.js
+++ app/js/directives/input-select.js
@@ -6,15 +6,8 @@ define(['./index'], function (directives) {
    
         link: function(scope, element, attrs) {
           element.bind('click', function() {
-            // save default value to restore on blur
-            url = element.val();
             // highlight all text for easy copying
             element.select();
-          });
-
-          element.bind('blur', function() {
-            // restore original value
-            element.val(url);
           });
         }
       };
