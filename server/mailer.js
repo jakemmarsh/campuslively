60fd4207f42995cabf012103ccdde68a16989ea2
Jake Marsh <jakemmarsh@gmail.com>
2014-02-13 14:38:26 -0500

    added matt back as a contact email recipient

--- server/mailer.js
+++ server/mailer.js
@@ -392,7 +392,7 @@ exports.sendContactEmail = function(req, res) {
         var deferred = Q.defer(),
             mailOptions = {
                 from: "Campuslively <jake@campuslively.com>",
-                to: "jake@campuslively.com",
+                to: "jake@campuslively.com, matt@campuslively.com",
                 html: '<strong>Message:</strong> ' + message.body + '<br /><br />'
                       + '<strong>Reply Email:</strong> ' + message.replyAddress
             };
