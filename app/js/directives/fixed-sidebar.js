093cf8967b4986aaecf5f7bc374a98218677dc4d
Jake Marsh <jakemmarsh@gmail.com>
2014-02-17 16:41:38 -0500

    redesigned and refactored sidebar to fit more icons, expand/show words only upon hover

--- app/js/directives/fixed-sidebar.js
+++ app/js/directives/fixed-sidebar.js
@@ -3,6 +3,35 @@ define(['./index'], function (directives) {
   // expand input and show post button on focus
   directives.directive('fixedSidebar', function() {
     return function(scope, element, attrs) {
+    	$(element).hover(
+    		// expand sidebar and slide over main content on hover
+    		function() {
+	    		$(element).css({
+	    			'width': '150px'
+	    		});
+
+	    		$(element).siblings('.main').css({
+	    			'-ms-transform': 'translate(100px, 0)',
+	    			'moz-transform': 'translate(100px, 0)',
+	    			'-webkit-transform': 'translate3d(100px, 0, 0)',
+	    			'-transform': 'translate(150px, 0)'
+	    		});
+    		},
+    		// shrink sidebar and slide main content back over on mouseleave
+    		function() {
+    			$(element).css({
+	    			'width': '50px'
+	    		});
+
+	    		$(element).siblings('.main').css({
+	    			'-ms-transform': 'none',
+	    			'moz-transform': 'none',
+	    			'-webkit-transform': 'none',
+	    			'-transform': 'none'
+	    		});
+    		}
+    	);
+
         angular.element(window).bind("scroll", function() {
              element.css('left', -this.pageXOffset);
         });
