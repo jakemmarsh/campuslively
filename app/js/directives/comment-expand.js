8d32c9e1d4a3f064c2ca3fa46f65631ed9c7ddae
Jake Marsh <jakemmarsh@gmail.com>
2014-02-18 13:11:25 -0500

    fixed line-height issue on expanding comment box

--- app/js/directives/comment-expand.js
+++ app/js/directives/comment-expand.js
@@ -6,18 +6,28 @@ define(['./index'], function (directives) {
       restrict: 'A',
  
       link: function(scope, element, attrs) {
+        $(element).css({
+          'line-height': '30px'
+        });
+
         element.bind('focus', expand);
         element.bind('blur', contract);
  
         function expand() {
           $(element).css('height', '150px');
           $(element).siblings('.btn').removeClass('hidden');
+          $(element).css({
+            'line-height': '18px'
+          });
         }
 
         function contract() {
           if(!$(element).val()) {
             $(element).height(33);
             $(element).siblings('.btn').addClass('hidden');
+            $(element).css({
+              'line-height': '30px'
+            });
           }
         }
       }
