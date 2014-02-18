02d3d3c91e3861f953ae7f9aade3ea11c81a26e4
Jake Marsh <jakemmarsh@gmail.com>
2014-02-18 12:25:11 -0500

    temporarily disabling unique email requirement

--- server/models/user.js
+++ server/models/user.js
@@ -42,8 +42,8 @@ var userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        required: true,
-        unique: true
+        required: true/*,
+        unique: true*/
     },
     firstName: {
         type: String,
